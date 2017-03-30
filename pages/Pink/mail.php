e7d8435c324f085f8754faaabbbd20fb1d6d94ba
Marginall <mistmee@mail.ru>
2017-03-30 23:33:46 +0300

    fix

--- pages/Pink/mail.php
+++ pages/Pink/mail.php
@@ -1,12 +1,14 @@
 <?php
 
-$recepient = "mistmee@mail.ru";
+$recepient = "mistmee2016@gmail.com";
 $sitename = "Название сайта";
 
 $name = trim($_POST["name"]);
-$phone = trim($_POST["phone"]);
 $text = trim($_POST["text"]);
-$message = "Имя: $name \nТелефон: $phone \nТекст: $text";
+$filename = $_FILES['images']['name'];
+$filetype = $_FILES['fileFF']['type'];
+
+$message = "Имя: $name \nТекст: $text \n$filename $filetype";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
