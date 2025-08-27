82805a6cb0aab40b16c20533f5cf1af0b544710e
Marginall <mistmee2016@gmail.com>
2025-08-27 20:18:25 +0300

    Updates

--- src/components/Forcast/types.ts
+++
@@ -1,5 +0,0 @@
-import { IForcast } from '../../types';
-
-export interface MainProps {
-	weatherData: IForcast | null;
-}
--- src/constants/index.ts
+++
@@ -1,2 +0,0 @@
-export const API_KEY = "e234ee70cb08407ca40135218250201";
-export const DEFAULT_CITY = "london";
